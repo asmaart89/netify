781a2fa12e4e459ce177f5eb38102b3c3e5206a4
asmaart89 <60203811+asmaart89@users.noreply.github.com>
2020-05-10 12:13:12 -0500

    a

---
+++ scripts/my-scripts.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    //alert("My Alert");
+  
+    //this is for the carousel. Change interval to manipulate speed  
+    $('.carousel').carousel({
+      interval: 20000
+    });
+  
+});
+
+
